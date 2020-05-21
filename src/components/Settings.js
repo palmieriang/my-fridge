895799e25668800a03d7bbd20ac97e06d40b3923
Angelo <angelo.palmieri@rufusleonard.com>
2020-05-21 14:52:32 +0100

    Localization

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,10 +1,24 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { useContext } from 'react';
+import { Button, Text, View } from 'react-native';
+import { LocalizationContext } from '../localization/localization';
 
 const Settings = () => {
+    const { t, locale, setLocale } = useContext(LocalizationContext);
+
     return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
             <Text>Settings</Text>
+            <Text>
+                Current locale: {locale}.{' '}
+                {locale !== 'en' && locale !== 'it'
+                ? 'Translations will fall back to "en" because none available'
+                : null}
+            </Text>
+            {locale === 'en' ? (
+                <Button title="Switch to Italian" onPress={() => setLocale('it')} />
+            ) : (
+                <Button title="Switch to English" onPress={() => setLocale('en')} />
+            )}
         </View>
     );
 }
