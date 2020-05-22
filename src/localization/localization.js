bf3c401bfad0d1bb2413a888041cd9ecaed9b3a9
Angelo <angelo.palmieri@rufusleonard.com>
2020-05-22 12:18:33 +0100

    Localization

--- src/localization/localization.js
+++ src/localization/localization.js
@@ -4,43 +4,8 @@ import i18n from 'i18n-js';
 
 export const LocalizationContext = createContext();
 
-const en = {
-  title: 'My fridge',
-  settings: 'Settings',
-  foo: 'Foo',
-  bar: 'Bar {{someValue}}',
-  product: 'Product name',
-  date: 'Expiring date',
-  choosePlace: 'Where do you want to add it?',
-  add: 'Add',
-  delete: 'Delete',
-  fridge: 'Fridge',
-  freezer: 'Freezer',
-  days: 'Days',
-  hours: 'Hours',
-  minutes: 'Minutes',
-  seconds: 'Seconds',
-  addItem: 'Add item',
-};
-
-const it = {
-  title: 'Il mio frigo',
-  settings: 'Impostazioni',
-  foo: 'Fou',
-  bar: 'Bár {{someValue}}',
-  product: 'Nome del prodotto',
-  date: 'Data di scadenza',
-  choosePlace: 'Dove vuoi aggiungerlo?',
-  add: 'Aggiungi',
-  delete: 'Rimuovi',
-  fridge: 'Frigo',
-  freezer: 'Freezer',
-  days: 'Giorni',
-  hours: 'Ore',
-  minutes: 'Minuti',
-  seconds: 'Secondi',
-  addItem: 'Aggiungi',
-};
+const en = require('./en.json');
+const it = require('./it.json');
 
 i18n.fallbacks = true;
 i18n.translations = { it, en };
