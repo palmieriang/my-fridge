dde1f15d4c16acb27e967249b13a052eaf2b4084
Angelo <angelo.palmieri@rufusleonard.com>
2020-05-25 13:11:33 +0100

    ProductCard

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -6,12 +6,7 @@ import { LocalizationContext } from '../localization/localization';
 
 const ProductCard = ({ product, changeProduct }) => {
     const { t } = useContext(LocalizationContext);
-    const {
-        days,
-        hours,
-        minutes,
-        seconds,
-    } = getCountdownParts(product.date);
+    const { days } = getCountdownParts(product.date);
 
     const handleChange = () => {
         changeProduct(product.id);
@@ -21,37 +16,11 @@ const ProductCard = ({ product, changeProduct }) => {
          <View>
             <TouchableHighlight onPress={handleChange} >
                 <View style={styles.card}>
-                    <View style={styles.cardHeader}>
-                        <Text style={styles.date}>{formatDate(product.date)}</Text>
-                        <Text style={styles.title}>{product.name}</Text>
-                    </View>
-                    <View
-                        style={styles.counterContainer}
-                    >
-                        <View
-                            style={styles.counter}
-                        >
-                            <Text style={styles.counterText}>{days}</Text>
-                            <Text style={styles.counterLabel}>{t('days').toUpperCase()}</Text>
-                        </View>
-                        <View
-                            style={styles.counter}
-                        >
-                            <Text style={styles.counterText}>{hours}</Text>
-                            <Text style={styles.counterLabel}>{t('hours').toUpperCase()}</Text>
-                        </View>
-                        <View
-                            style={styles.counter}
-                        >
-                            <Text style={styles.counterText}>{minutes}</Text>
-                            <Text style={styles.counterLabel}>{t('minutes').toUpperCase()}</Text>
-                        </View>
-                        <View
-                            style={styles.counter}
-                        >
-                            <Text style={styles.counterText}>{seconds}</Text>
-                            <Text style={styles.counterLabel}>{t('seconds').toUpperCase()}</Text>
-                        </View>
+                    <Text style={styles.date}>{formatDate(product.date)}</Text>
+                    <Text style={styles.title}>{product.name}</Text>
+                    <View style={styles.counterContainer}>
+                        <Text style={styles.counterText}>{days}</Text>
+                        <Text style={styles.counterLabel}>{t('days').toUpperCase()}</Text>
                     </View>
                 </View>
             </TouchableHighlight>
@@ -70,51 +39,33 @@ const styles = StyleSheet.create({
     card: {
         backgroundColor: '#fff',
         flex: 1,
-        padding: 10,
-        paddingTop: 10,
-        paddingBottom: 20,
+        padding: 15,
         margin: 10,
         marginTop: 5,
         marginBottom: 5,
     },
-    cardHeader: {
-        flex: 1,
-        flexDirection: 'row',
-    },
     date: {
         fontWeight: '200',
         fontSize: 15,
-        color: '#bdbdbd',
-        width: '30%',
-        textAlign: 'right',
+        marginBottom: 10,
     },
     title: {
         fontSize: 15,
         fontWeight: '300',
-        marginLeft: 7,
-        textAlign: 'left',
+        marginBottom: 10,
     },
     counterContainer: {
         flex: 1,
         flexDirection: 'row',
-        justifyContent: 'space-between',
-        paddingLeft: '5%',
-        paddingRight: '5%',
-    },
-    counter: {
-        width: '25%',
-        flex: 1,
+        alignItems: 'baseline',
     },
     counterText: {
         fontSize: 40,
-        textAlign: 'center',
     },
     counterLabel: {
         fontSize: 13,
         fontWeight: '100',
-        color: '#a3a3a3',
-        textAlign: 'center',
-        paddingTop: 0,
+        marginLeft: 10,
     },
 });
 
