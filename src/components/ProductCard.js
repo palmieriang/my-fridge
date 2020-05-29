760e0bae37d93c5c411ff93d304de63bfdda67be
Angelo <angelo.palmieri@rufusleonard.com>
2020-05-29 17:41:11 +0100

    Swipeable style

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,9 +1,9 @@
 import React, { useContext } from 'react';
-import { Text, TouchableHighlight, View, StyleSheet } from 'react-native';
+import { Animated, Text, TouchableHighlight, View, StyleSheet, I18nManager } from 'react-native';
 import PropTypes from 'prop-types';
 import { formatDate, getCountdownParts } from '../../api/api';
 import { LocalizationContext } from '../localization/localization';
-import { Swipeable } from 'react-native-gesture-handler';
+import { RectButton, Swipeable } from 'react-native-gesture-handler';
 
 const ProductCard = ({ product, changeProduct, deleteProduct }) => {
     const { t } = useContext(LocalizationContext);
@@ -15,31 +15,76 @@ const ProductCard = ({ product, changeProduct, deleteProduct }) => {
 
     const handleDeletePress = () => {
         console.log('Item deleted from swipe');
-        deleteProduct(product.id);
+        // deleteProduct(product.id);
     }
 
-    const handleLeftAction = () => {
+    const renderLeftActions = (progress, dragX) => {
+        const trans = dragX.interpolate({
+            inputRange: [0, 50, 100, 101],
+            outputRange: [-20, 0, 0, 1],
+        });
+
         return (
-            <View style={styles.swipe}>
-                <Text>Completed</Text>
-            </View>
+            <RectButton style={styles.leftAction} onPress={close}>
+                <Animated.Text
+                style={[
+                    styles.actionText,
+                    {
+                    transform: [{ translateX: trans }],
+                    },
+                ]}>
+                Archive
+                </Animated.Text>
+            </RectButton>
         );
-    }
+    };
 
-    const handleRightAction = () => {
+    const renderRightAction = (text, color, x, progress) => {
+        const trans = progress.interpolate({
+            inputRange: [0, 1],
+            outputRange: [x, 0],
+        });
+
+        const pressHandler = () => {
+            close();
+            alert(text);
+        };
+
         return (
-            <View style={styles.swipe}>
-                <Text>Removed</Text>
-            </View>
+          <Animated.View style={{ flex: 1, transform: [{ translateX: trans }] }}>
+            <RectButton
+              style={[styles.rightAction, { backgroundColor: color }]}
+              onPress={pressHandler}>
+              <Text style={styles.actionText}>{text}</Text>
+            </RectButton>
+          </Animated.View>
         );
-    }
+    };
 
+    const renderRightActions = progress => (
+        <View style={{ width: 192, flexDirection: I18nManager.isRTL? 'row-reverse' : 'row' }}>
+            {renderRightAction('More', '#C8C7CD', 192, progress)}
+            {renderRightAction('Flag', '#ffab00', 128, progress)}
+            {renderRightAction('More', '#dd2c00', 64, progress)}
+        </View>
+    );
+
+    const updateRef = ref => {
+        _swipeableRow = ref;
+    };
+
+    const close = () => {
+        _swipeableRow.close();
+    };
     return (
         <Swipeable
-            renderLeftActions={handleLeftAction}
-            renderRightActions={handleRightAction}
-            onSwipeableLeftOpen={handleDeletePress}
-            onSwipeableRightOpen={() => console.log('closing')}
+            ref={updateRef}
+            renderLeftActions={renderLeftActions}
+            renderRightActions={renderRightActions}
+            onSwipeableLeftOpen={() => console.log('closing')}
+            onSwipeableRightOpen={handleDeletePress}
+            leftThreshold={30}
+            rightThreshold={40}
         >
             <View>
                 <TouchableHighlight onPress={handleChange} >
@@ -96,13 +141,26 @@ const styles = StyleSheet.create({
         fontWeight: '100',
         marginLeft: 10,
     },
-    swipe: {
-        backgroundColor: 'green',
-        // flex: 1,
-        alignContent: 'center',
+    leftAction: {
+        flex: 1,
+        backgroundColor: '#497AFC',
+        justifyContent: 'center',
         marginTop: 5,
         marginBottom: 5,
-    }
+    },
+    actionText: {
+        color: 'white',
+        fontSize: 16,
+        backgroundColor: 'transparent',
+        padding: 10,
+    },
+    rightAction: {
+        alignItems: 'center',
+        flex: 1,
+        justifyContent: 'center',
+        marginTop: 5,
+        marginBottom: 5,
+    },
 });
 
 export default ProductCard;
