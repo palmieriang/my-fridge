98488be8caa570ca90cea1225db72d619c9a463a
Angelo <angelo.palmieri@rufusleonard.com>
2020-06-21 13:39:58 +0100

    api.js - save - modify

--- api/api.js
+++ api/api.js
@@ -1,5 +1,43 @@
 import 'react-native-get-random-values';
 import moment from 'moment';
+import { firebase } from '../src/firebase/config';
+
+const productRef = firebase.firestore().collection('products');
+
+export function saveProduct({ name, date, place, authorID }) {
+  const timestamp = firebase.firestore.FieldValue.serverTimestamp();
+  const data = {
+    name,
+    date,
+    place,
+    authorID,
+    createdAt: timestamp,
+  };
+  return productRef
+    .doc()
+    .set(data)
+    .catch((error) => {
+      alert(error)
+    });
+}
+
+export function modifyProduct({ name, date, place, authorID }, existingId) {
+  console.info(name, date, place, authorID, existingId);
+  const timestamp = firebase.firestore.FieldValue.serverTimestamp();
+  const data = {
+    name,
+    date,
+    place,
+    authorID,
+    createdAt: timestamp,
+  };
+  return productRef
+    .doc(existingId)
+    .set(data)
+    .catch((error) => {
+      alert(error)
+    });
+}
 
 export function formatDate(dateString) {
   const parsed = moment(new Date(dateString));
