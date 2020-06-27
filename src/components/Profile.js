17bd7d6d5ad328644fe60dc8a3f9eb659871d038
Angelo <angelo.palmieri@rufusleonard.com>
2020-06-27 15:22:44 +0100

    Profile

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -26,13 +26,11 @@ const Profile = () => {
 
     const pickImage = async () => {
         let result = await ImagePicker.launchImageLibraryAsync({
-            mediaTypes: ImagePicker.MediaTypeOptions.All,
+            mediaTypes: ImagePicker.MediaTypeOptions.Images,
             allowsEditing: true,
             aspect: [4, 3],
             quality: 1,
         });
-
-        let filename = result.uri.split('/').pop();
 
         if (!result.cancelled) {
             setImage(result.uri);
