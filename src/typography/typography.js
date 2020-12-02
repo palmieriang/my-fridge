3ed8cbdf6074ba3c85d605081ad6ed4492eadbd6
Angelo <angelo.palmieri@rufusleonard.com>
2020-12-02 15:46:59 +0000

    ProductCard - fontFamily

--- src/typography/typography.js
+++ src/typography/typography.js
@@ -1,3 +1,6 @@
 export const fontsList = {
   NerkoOneRegular: require('../../assets/fonts/Nerko_One/NerkoOne-Regular.ttf'),
+  OpenSansRegular: require('../../assets/fonts/Open_Sans/OpenSans-Regular.ttf'),
+  OpenSansBold: require('../../assets/fonts/Open_Sans/OpenSans-Bold.ttf'),
+  OpenSansLight: require('../../assets/fonts/Open_Sans/OpenSans-Light.ttf'),
 };
