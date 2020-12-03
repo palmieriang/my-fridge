b95475c559e1335160509fb506f9c43af8f97a52
Angelo <angelo.palmieri@rufusleonard.com>
2020-12-03 09:31:26 +0000

    Registration - fontFamily

--- src/components/Registration.js
+++ src/components/Registration.js
@@ -1,136 +1,143 @@
 import React, { useState, useContext } from 'react';
 import {
-    StyleSheet,
-    Text,
-    TextInput,
-    TouchableOpacity,
-    View
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
 } from 'react-native';
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 
 import { authStore } from '../store/authStore';
 
 const Registration = ({ navigation }) => {
-    const [fullName, setFullName] = useState('')
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [confirmPassword, setConfirmPassword] = useState('')
+  const [fullName, setFullName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
 
-    const { authContext } = useContext(authStore);
+  const { authContext } = useContext(authStore);
 
-    const handleRegistration = () => {
-        authContext
-            .signUp({ fullName, email, password, confirmPassword })
-            .then(() => navigation.navigate('signin'));
-    }
+  const handleRegistration = () => {
+    authContext
+      .signUp({ fullName, email, password, confirmPassword })
+      .then(() => navigation.navigate('signin'));
+  };
 
-    const handleGoToLogin = () => {
-        navigation.navigate('signin');
-    }
+  const handleGoToLogin = () => {
+    navigation.navigate('signin');
+  };
 
-    return (
-        <View style={styles.container}>
-            <KeyboardAwareScrollView
-                style={{ flex: 1, width: '100%' }}
-                keyboardShouldPersistTaps="always"
-            >
-                <TextInput
-                    style={styles.input}
-                    placeholder="Full Name"
-                    placeholderTextColor="#aaaaaa"
-                    onChangeText={setFullName}
-                    value={fullName}
-                    underlineColorAndroid="transparent"
-                    autoCapitalize="none"
-                />
-                <TextInput
-                    style={styles.input}
-                    placeholder="E-mail"
-                    placeholderTextColor="#aaaaaa"
-                    onChangeText={setEmail}
-                    value={email}
-                    underlineColorAndroid="transparent"
-                    autoCapitalize="none"
-                />
-                <TextInput
-                    style={styles.input}
-                    placeholderTextColor="#aaaaaa"
-                    secureTextEntry
-                    placeholder="Password"
-                    onChangeText={setPassword}
-                    value={password}
-                    underlineColorAndroid="transparent"
-                    autoCapitalize="none"
-                />
-                <TextInput
-                    style={styles.input}
-                    placeholderTextColor="#aaaaaa"
-                    secureTextEntry
-                    placeholder="Confirm Password"
-                    onChangeText={setConfirmPassword}
-                    value={confirmPassword}
-                    underlineColorAndroid="transparent"
-                    autoCapitalize="none"
-                />
-                <TouchableOpacity
-                    style={styles.button}
-                    onPress={handleRegistration}
-                >
-                    <Text style={styles.buttonTitle}>Create account</Text>
-                </TouchableOpacity>
-                <View style={styles.footerView}>
-                    <Text style={styles.footerText}>Already got an account? <Text onPress={handleGoToLogin} style={styles.footerLink}>Log in</Text></Text>
-                </View>
-            </KeyboardAwareScrollView>
+  return (
+    <View style={styles.container}>
+      <KeyboardAwareScrollView
+        style={{ flex: 1, width: '100%' }}
+        keyboardShouldPersistTaps="always"
+      >
+        <TextInput
+          style={styles.input}
+          placeholder="Full Name"
+          placeholderTextColor="#aaaaaa"
+          onChangeText={setFullName}
+          value={fullName}
+          underlineColorAndroid="transparent"
+          autoCapitalize="none"
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="E-mail"
+          placeholderTextColor="#aaaaaa"
+          onChangeText={setEmail}
+          value={email}
+          underlineColorAndroid="transparent"
+          autoCapitalize="none"
+        />
+        <TextInput
+          style={styles.input}
+          placeholderTextColor="#aaaaaa"
+          secureTextEntry
+          placeholder="Password"
+          onChangeText={setPassword}
+          value={password}
+          underlineColorAndroid="transparent"
+          autoCapitalize="none"
+        />
+        <TextInput
+          style={styles.input}
+          placeholderTextColor="#aaaaaa"
+          secureTextEntry
+          placeholder="Confirm Password"
+          onChangeText={setConfirmPassword}
+          value={confirmPassword}
+          underlineColorAndroid="transparent"
+          autoCapitalize="none"
+        />
+        <TouchableOpacity style={styles.button} onPress={handleRegistration}>
+          <Text style={styles.buttonTitle}>Create account</Text>
+        </TouchableOpacity>
+        <View style={styles.footerView}>
+          <Text style={styles.footerText}>
+            Already got an account?{' '}
+            <Text onPress={handleGoToLogin} style={styles.footerLink}>
+              Log in
+            </Text>
+          </Text>
         </View>
-    );
+      </KeyboardAwareScrollView>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        alignItems: 'center',
-        flex: 1,
-    },
-    input: {
-        backgroundColor: 'white',
-        borderRadius: 5,
-        height: 48,
-        marginTop: 10,
-        marginBottom: 10,
-        marginLeft: 30,
-        marginRight: 30,
-        overflow: 'hidden',
-        paddingLeft: 16,
-    },
-    button: {
-        alignItems: "center",
-        backgroundColor: '#48BBEC',
-        borderRadius: 5,
-        justifyContent: 'center',
-        height: 48,
-        marginLeft: 30,
-        marginRight: 30,
-        marginTop: 20,
-    },
-    buttonTitle: {
-        color: 'white',
-        fontSize: 16,
-        fontWeight: 'bold',
-    },
-    footerView: {
-        alignItems: 'center',
-        flex: 1,
-        marginTop: 20,
-    },
-    footerText: {
-        color: '#2e2e2d',
-        fontSize: 16,
-    },
-    footerLink: {
-        color: '#48BBEC',
-        fontSize: 16,
-        fontWeight: 'bold',
-    },
+  container: {
+    alignItems: 'center',
+    flex: 1,
+  },
+  input: {
+    backgroundColor: 'white',
+    borderRadius: 5,
+    fontFamily: 'OpenSansRegular',
+    height: 48,
+    marginTop: 10,
+    marginBottom: 10,
+    marginLeft: 30,
+    marginRight: 30,
+    overflow: 'hidden',
+    paddingLeft: 16,
+  },
+  button: {
+    alignItems: 'center',
+    backgroundColor: '#48BBEC',
+    borderRadius: 5,
+    justifyContent: 'center',
+    height: 48,
+    marginLeft: 30,
+    marginRight: 30,
+    marginTop: 20,
+  },
+  buttonTitle: {
+    color: 'white',
+    fontFamily: 'OpenSansBold',
+    fontSize: 16,
+    fontWeight: 'bold',
+    textTransform: 'uppercase',
+  },
+  footerView: {
+    alignItems: 'center',
+    flex: 1,
+    marginTop: 20,
+  },
+  footerText: {
+    color: '#2e2e2d',
+    fontFamily: 'OpenSansRegular',
+    fontSize: 16,
+  },
+  footerLink: {
+    color: '#48BBEC',
+    fontFamily: 'OpenSansBold',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
 });
 
 export default Registration;
