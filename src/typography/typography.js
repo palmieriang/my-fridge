ab8b4af99ab6e9f5690c3cff920a560862d5feb3
Angelo <angelo.palmieri@rufusleonard.com>
2020-12-03 09:28:43 +0000

    typography - Nunito - LilitaOne

--- src/typography/typography.js
+++ src/typography/typography.js
@@ -1,5 +1,8 @@
-export const fontsList = {
-  NerkoOneRegular: require('../../assets/fonts/Nerko_One/NerkoOne-Regular.ttf'),
+export const customFonts = {
+  LilitaOne: require('../../assets/fonts/Lilita_One/LilitaOne-Regular.ttf'),
+  NunitoRegular: require('../../assets/fonts/Nunito/Nunito-Regular.ttf'),
+  NunitoBold: require('../../assets/fonts/Nunito/Nunito-Bold.ttf'),
+  NunitoLight: require('../../assets/fonts/Nunito/Nunito-Light.ttf'),
   OpenSansRegular: require('../../assets/fonts/Open_Sans/OpenSans-Regular.ttf'),
   OpenSansBold: require('../../assets/fonts/Open_Sans/OpenSans-Bold.ttf'),
   OpenSansLight: require('../../assets/fonts/Open_Sans/OpenSans-Light.ttf'),
