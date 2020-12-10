652a89e95c37a94b75d0dee2266946616d00a43a
Angelo <angelo.palmieri@rufusleonard.com>
2020-12-10 23:24:17 +0000

    api - signInWithGoogle

--- api/api.js
+++ api/api.js
@@ -1,11 +1,13 @@
 import 'react-native-get-random-values';
 import moment from 'moment';
+import * as Google from 'expo-google-app-auth';
 import {
   firebase,
   userRef,
   productRef,
   imagesRef,
 } from '../src/firebase/config';
+import { IOS_CLIENT_ID } from '@env';
 
 // Auth
 
@@ -50,6 +52,79 @@ export function authSignIn(email, password) {
   });
 }
 
+function isUserEqual(googleUser, firebaseUser) {
+  if (firebaseUser) {
+    var providerData = firebaseUser.providerData;
+    for (var i = 0; i < providerData.length; i++) {
+      if (
+        providerData[i].providerId ===
+          firebase.auth.GoogleAuthProvider.PROVIDER_ID &&
+        providerData[i].uid === googleUser.getBasicProfile().getId()
+      ) {
+        // We don't need to reauth the Firebase connection.
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+function onSignIn(googleUser) {
+  // We need to register an Observer on Firebase Auth to make sure auth is initialized.
+  var unsubscribe = firebase.auth().onAuthStateChanged((firebaseUser) => {
+    unsubscribe();
+    // Check if we are already signed-in Firebase with the correct user.
+    if (!isUserEqual(googleUser, firebaseUser)) {
+      // Build Firebase credential with the Google ID token.
+      var credential = firebase.auth.GoogleAuthProvider.credential(
+        googleUser.idToken,
+        googleUser.accessToken
+      );
+
+      // Sign in with credential from the Google user.
+      firebase
+        .auth()
+        .signInWithCredential(credential)
+        .then((result) => {
+          const uid = result.user.uid;
+          const data = {
+            uid,
+            email: result.user.email,
+            fullName: result.additionalUserInfo.profile.name,
+            locale: result.additionalUserInfo.profile.locale,
+            profileImg: result.additionalUserInfo.profile.picture,
+            theme: 'lightRed',
+          };
+          addUserData(uid, data);
+        })
+        .catch((error) => {
+          console.log('onSignIn error', error);
+        });
+    } else {
+      console.log('User already signed-in Firebase.');
+    }
+  });
+}
+
+export async function signInWithGoogle() {
+  let result;
+  try {
+    result = await Google.logInAsync({
+      iosClientId: IOS_CLIENT_ID,
+    });
+  } catch ({ message }) {
+    console.log('Google.logInAsync(): ' + message);
+  }
+
+  if (result.type === 'success') {
+    onSignIn(result);
+    const { idToken, user } = result; // or accessToken??
+    return { idToken, user };
+  } else {
+    console.log('Cancelled');
+  }
+}
+
 export function authSignOut() {
   return firebase.auth().signOut();
 }
@@ -78,7 +153,7 @@ export function addUserData(uid, data) {
   return userRef
     .doc(uid)
     .set(data)
-    .catch((error) => console.log('Error: ', error));
+    .catch((error) => console.log('Error adding user data: ', error));
 }
 
 export function getUserData(userID) {
