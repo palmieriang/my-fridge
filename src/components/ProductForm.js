16285caa87146501e8d9c6a7fda7f295663742b1
Angelo <angelo.palmieri@rufusleonard.com>
2021-02-01 09:27:35 +0000

    ProductForm

--- src/components/ProductForm.js
+++ src/components/ProductForm.js
@@ -163,23 +163,29 @@ const ProductForm = ({ navigation, route }) => {
 const pickerSelectStyles = StyleSheet.create({
   inputIOS: {
     borderColor: '#ccc',
+    fontSize: adjust(14),
     height: '100%',
     paddingLeft: 66,
   },
   inputAndroid: {
     borderColor: '#ccc',
+    fontSize: adjust(14),
     height: '100%',
     marginLeft: 60,
     paddingLeft: 66,
   },
   iconContainer: {
-    left: 0,
-    width: 54,
-    height: '100%',
-    justifyContent: 'center',
     alignItems: 'center',
     borderRightColor: '#ccc',
     borderRightWidth: StyleSheet.hairlineWidth,
+    fontSize: adjust(14),
+    justifyContent: 'center',
+    height: '100%',
+    left: 0,
+    width: 54,
+  },
+  placeholder: {
+    color: '#aaaaaa',
   },
 });
 
