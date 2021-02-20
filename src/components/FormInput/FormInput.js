36426c669f875a9b532deaa2720a0bfb18aeb76c
Angelo <angelo.palmieri@rufusleonard.com>
2021-02-20 18:07:28 +0000

    FormInput

--- src/components/FormInput/FormInput.js
+++ src/components/FormInput/FormInput.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { View, TextInput, StyleSheet } from 'react-native';
-import { adjust } from './utils/dimensions';
+import { View, TextInput } from 'react-native';
+import styles from './styles';
 
 const FormInput = ({ labelValue, placeholderText, Icon, ...rest }) => {
   return (
@@ -25,38 +25,6 @@ const FormInput = ({ labelValue, placeholderText, Icon, ...rest }) => {
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  inputContainer: {
-    alignItems: 'center',
-    backgroundColor: '#fff',
-    borderColor: '#ccc',
-    borderRadius: 5,
-    borderWidth: StyleSheet.hairlineWidth,
-    flex: 1,
-    flexDirection: 'row',
-    marginTop: 10,
-    marginBottom: 10,
-    marginLeft: 30,
-    marginRight: 30,
-    maxHeight: 56,
-    overflow: 'hidden',
-  },
-  iconStyle: {
-    alignItems: 'center',
-    borderRightColor: '#ccc',
-    borderRightWidth: StyleSheet.hairlineWidth,
-    padding: 14,
-  },
-  input: {
-    backgroundColor: 'white',
-    flex: 1,
-    fontFamily: 'OpenSans-Regular',
-    fontSize: adjust(14),
-    justifyContent: 'center',
-    padding: 14,
-  },
-});
 
 FormInput.propTypes = {
   labelValue: PropTypes.string.isRequired,
---
+++ src/components/FormInput/styles.js
@@ -0,0 +1,34 @@
+import { StyleSheet } from 'react-native';
+import { adjust } from '../utils/dimensions';
+
+export default StyleSheet.create({
+  inputContainer: {
+    alignItems: 'center',
+    backgroundColor: '#fff',
+    borderColor: '#ccc',
+    borderRadius: 5,
+    borderWidth: StyleSheet.hairlineWidth,
+    flex: 1,
+    flexDirection: 'row',
+    marginTop: 10,
+    marginBottom: 10,
+    marginLeft: 30,
+    marginRight: 30,
+    maxHeight: 56,
+    overflow: 'hidden',
+  },
+  iconStyle: {
+    alignItems: 'center',
+    borderRightColor: '#ccc',
+    borderRightWidth: StyleSheet.hairlineWidth,
+    padding: 14,
+  },
+  input: {
+    backgroundColor: 'white',
+    flex: 1,
+    fontFamily: 'OpenSans-Regular',
+    fontSize: adjust(14),
+    justifyContent: 'center',
+    padding: 14,
+  },
+});
