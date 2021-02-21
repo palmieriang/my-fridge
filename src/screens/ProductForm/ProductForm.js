1d28fd1d3fb5b5bc6808c4f580d7800a32e8a294
Angelo <angelo.palmieri@rufusleonard.com>
2021-02-21 13:40:02 +0000

    ProductForm

--- src/screens/ProductForm/ProductForm.js
+++ src/screens/ProductForm/ProductForm.js
@@ -3,16 +3,17 @@ import { Text, TouchableOpacity, View, StyleSheet, LogBox } from 'react-native';
 import DateTimePickerModal from 'react-native-modal-datetime-picker';
 import RNPickerSelect from 'react-native-picker-select';
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
-import FormInput from '../components/FormInput';
-import { formatDate } from '../../api/api';
-import { adjust } from '../components/utils/dimensions';
-import { localeStore } from '../store/localeStore';
-import { authStore } from '../store/authStore';
-import { themeStore } from '../store/themeStore';
-import { productsStore } from '../store/productsStore';
-import CalendarIcon from '../components/svg/CalendarIcon';
-import ColdIcon from '../components/svg/ColdIcon';
-import ShoppingBasketIcon from '../components/svg/ShoppingBasketIcon';
+import FormInput from '../../components/FormInput/FormInput';
+import { formatDate } from '../../../api/api';
+import { adjust } from '../../components/utils/dimensions';
+import { localeStore } from '../../store/localeStore';
+import { authStore } from '../../store/authStore';
+import { themeStore } from '../../store/themeStore';
+import { productsStore } from '../../store/productsStore';
+import CalendarIcon from '../../components/svg/CalendarIcon';
+import ColdIcon from '../../components/svg/ColdIcon';
+import ShoppingBasketIcon from '../../components/svg/ShoppingBasketIcon';
+import styles from './styles';
 
 LogBox.ignoreLogs([
   'Non-serializable values were found in the navigation state',
@@ -190,48 +191,6 @@ const pickerSelectStyles = StyleSheet.create({
   },
   placeholder: {
     color: '#757575',
-  },
-});
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    width: '100%',
-  },
-  inputContainer: {
-    backgroundColor: '#fff',
-    borderColor: '#ccc',
-    borderRadius: 5,
-    borderWidth: StyleSheet.hairlineWidth,
-    flex: 1,
-    marginTop: 10,
-    marginBottom: 10,
-    marginLeft: 30,
-    marginRight: 30,
-    maxHeight: 54,
-    overflow: 'hidden',
-  },
-  button: {
-    alignItems: 'center',
-    backgroundColor: '#48BBEC',
-    borderRadius: 5,
-    justifyContent: 'center',
-    height: 48,
-    marginLeft: 30,
-    marginRight: 30,
-    marginTop: 20,
-  },
-  buttonTitle: {
-    color: '#fff',
-    fontFamily: 'OpenSans-Bold',
-    fontSize: adjust(16),
-    textShadowColor: '#000',
-    textShadowOffset: { width: 0, height: 0 },
-    textShadowRadius: 1,
-    textTransform: 'uppercase',
-  },
-  buttonDelete: {
-    backgroundColor: '#e74c3c',
   },
 });
 
