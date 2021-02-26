97d7805f44704e445c46bdfdb38bb55e06a7275d
Angelo <angelo.palmieri@rufusleonard.com>
2021-02-26 08:33:17 +0000

    Settings

---
+++ src/screens/Settings/styles.js
@@ -0,0 +1,23 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  selectorContainer: {
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    marginTop: 20,
+    minWidth: 200,
+  },
+  initValueTextStyle: {
+    color: 'black',
+    fontFamily: 'OpenSans-Regular',
+  },
+  text: {
+    fontFamily: 'OpenSans-Regular',
+  },
+  container: {
+    backgroundColor: 'lightgrey',
+  },
+  logout: {
+    marginTop: 20,
+  },
+});
