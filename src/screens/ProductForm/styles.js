311fd055ac3b5f298792e8c2d5f61e11ba724f87
Angelo <angelo.palmieri@rufusleonard.com>
2021-03-03 07:56:58 +0000

    ProductForm

--- src/screens/ProductForm/styles.js
+++ src/screens/ProductForm/styles.js
@@ -1,5 +1,4 @@
 import { StyleSheet } from 'react-native';
-import { adjust } from '../../components/utils/dimensions';
 
 export default StyleSheet.create({
   container: {
@@ -18,27 +17,5 @@ export default StyleSheet.create({
     marginRight: 30,
     maxHeight: 54,
     overflow: 'hidden',
-  },
-  button: {
-    alignItems: 'center',
-    backgroundColor: '#48BBEC',
-    borderRadius: 5,
-    justifyContent: 'center',
-    height: 48,
-    marginLeft: 30,
-    marginRight: 30,
-    marginTop: 20,
-  },
-  buttonTitle: {
-    color: '#fff',
-    fontFamily: 'OpenSans-Bold',
-    fontSize: adjust(16),
-    textShadowColor: '#000',
-    textShadowOffset: { width: 0, height: 0 },
-    textShadowRadius: 1,
-    textTransform: 'uppercase',
-  },
-  buttonDelete: {
-    backgroundColor: '#e74c3c',
   },
 });
