e701216ceb2b5af3f087828880ec15b1e6082d50
Angelo <angelo.palmieri@rufusleonard.com>
2021-03-05 20:41:40 +0000

    config alias

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,17 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: ['module:react-native-dotenv'],
+    plugins: [
+      ['module:react-native-dotenv'],
+      [
+        'module-resolver',
+        {
+          alias: {
+            '@components': './src/components',
+            '@screens': './src/screens',
+          },
+        },
+      ],
+    ],
   };
 };
