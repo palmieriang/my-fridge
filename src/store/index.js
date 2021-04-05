54fd6a697524cbe9e60730f6c43cb37024e6d589
Angelo <angelo.palmieri@rufusleonard.com>
2021-04-05 09:04:16 +0100

    export store

---
+++ src/store/index.js
@@ -0,0 +1,4 @@
+export { AuthProvider, authStore } from './authStore';
+export { LocaleProvider, localeStore } from './localeStore';
+export { ProductsProvider, productsStore } from './productsStore';
+export { ThemeProvider, themeStore } from './themeStore';
