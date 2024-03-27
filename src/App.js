b01a945d85312210048307f617f9547f33cdd183
asos-angelopalmieri <angelo.palmieri@asos.com>
2024-03-27 21:15:53 +0000

    created AppContainer and preloading fonts before loading it

--- src/App.js
+++ src/App.js
@@ -1,35 +1,17 @@
 import "react-native-gesture-handler";
-import Icon from "@components/Icon/Icon";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { NavigationContainer } from "@react-navigation/native";
 import { registerRootComponent } from "expo";
 import { useFonts } from "expo-font";
 import React from "react";
 import { LogBox } from "react-native";
 
-import { FRIDGE, FREEZER, SETTINGS } from "./constants";
-import {
-  FridgeStackScreen,
-  FreezerStackScreen,
-  SettingsStackScreen,
-  SingInStackScreen,
-} from "./navigation/navigation";
-import {
-  AuthProvider,
-  LocaleProvider,
-  ProductsProvider,
-  ThemeProvider,
-} from "./store";
+import AppContainer from "./AppContainer";
 import { customFonts } from "./typography/typography";
-import { countExpiredItems } from "../src/utils";
 
 LogBox.ignoreLogs([
   "Warning: componentWillMount is deprecated",
   "Warning: componentWillReceiveProps has been renamed",
   "Warning: Setting a timer",
 ]);
-
-const Tab = createBottomTabNavigator();
 
 export default function App() {
   const [fontsLoaded] = useFonts(customFonts);
@@ -38,73 +20,7 @@ export default function App() {
     return null;
   }
 
-  return (
-    <AuthProvider>
-      {({ authState: { userToken } }) => (
-        <LocaleProvider>
-          {({ localizationContext: { t } }) => (
-            <ThemeProvider>
-              {({ theme: { primary } }) => (
-                <ProductsProvider>
-                  {({ productsList }) => (
-                    <NavigationContainer>
-                      {userToken ? (
-                        <Tab.Navigator
-                          screenOptions={({ route }) => ({
-                            tabBarIcon: ({ focused, color, size }) => (
-                              <Icon
-                                type={route.name.toLowerCase()}
-                                size={size}
-                                fill={color}
-                                focused={focused}
-                              />
-                            ),
-                          })}
-                          tabBarOptions={{
-                            ...tabBarOptions,
-                            activeTintColor: primary,
-                          }}
-                        >
-                          <Tab.Screen
-                            name={t(FRIDGE)}
-                            component={FridgeStackScreen}
-                            options={{
-                              tabBarBadge: countExpiredItems(productsList),
-                            }}
-                          />
-                          <Tab.Screen
-                            name={t(FREEZER)}
-                            component={FreezerStackScreen}
-                          />
-                          <Tab.Screen
-                            name={t(SETTINGS)}
-                            component={SettingsStackScreen}
-                          />
-                        </Tab.Navigator>
-                      ) : (
-                        <SingInStackScreen />
-                      )}
-                    </NavigationContainer>
-                  )}
-                </ProductsProvider>
-              )}
-            </ThemeProvider>
-          )}
-        </LocaleProvider>
-      )}
-    </AuthProvider>
-  );
+  return <AppContainer />;
 }
 
 registerRootComponent(App);
-
-const tabBarOptions = {
-  inactiveTintColor: "black",
-  showIcon: true,
-  showLabel: true,
-  upperCaseLabel: true,
-  labelStyle: {
-    fontFamily: "OpenSans-Regular",
-    textTransform: "uppercase",
-  },
-};
