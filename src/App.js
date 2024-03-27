9a88a8c5554de6e316f67706199528d5a3305ea6
asos-angelopalmieri <angelo.palmieri@asos.com>
2024-03-27 21:18:31 +0000

    Removed logbox

--- src/App.js
+++ src/App.js
@@ -2,16 +2,9 @@ import "react-native-gesture-handler";
 import { registerRootComponent } from "expo";
 import { useFonts } from "expo-font";
 import React from "react";
-import { LogBox } from "react-native";
 
 import AppContainer from "./AppContainer";
 import { customFonts } from "./typography/typography";
-
-LogBox.ignoreLogs([
-  "Warning: componentWillMount is deprecated",
-  "Warning: componentWillReceiveProps has been renamed",
-  "Warning: Setting a timer",
-]);
 
 export default function App() {
   const [fontsLoaded] = useFonts(customFonts);
