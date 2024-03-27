fbdc3b26a4616bc4a8099c4a7fe2462acfc98b96
asos-angelopalmieri <angelo.palmieri@asos.com>
2024-03-27 22:32:56 +0000

    created TabNavigator function

--- src/AppContainer.js
+++ src/AppContainer.js
@@ -1,27 +1,17 @@
 import "react-native-gesture-handler";
-import Icon from "@components/Icon/Icon";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { NavigationContainer } from "@react-navigation/native";
-import React from "react";
+import React, { memo } from "react";
 
-import { FRIDGE, FREEZER, SETTINGS } from "./constants";
-import {
-  FridgeStackScreen,
-  FreezerStackScreen,
-  SettingsStackScreen,
-  SingInStackScreen,
-} from "./navigation/navigation";
+import TabNavigator from "./TabNavigator";
+import { SingInStackScreen } from "./navigation/navigation";
 import {
   AuthProvider,
   LocaleProvider,
   ProductsProvider,
   ThemeProvider,
 } from "./store";
-import { countExpiredItems } from "../src/utils";
 
-const Tab = createBottomTabNavigator();
-
-export default function AppContainer() {
+function AppContainer() {
   return (
     <AuthProvider>
       {({ authState: { userToken } }) => (
@@ -33,38 +23,7 @@ export default function AppContainer() {
                   {({ productsList }) => (
                     <NavigationContainer>
                       {userToken ? (
-                        <Tab.Navigator
-                          screenOptions={({ route }) => ({
-                            tabBarIcon: ({ focused, color, size }) => (
-                              <Icon
-                                type={route.name.toLowerCase()}
-                                size={size}
-                                fill={color}
-                                focused={focused}
-                              />
-                            ),
-                          })}
-                          tabBarOptions={{
-                            ...tabBarOptions,
-                            activeTintColor: primary,
-                          }}
-                        >
-                          <Tab.Screen
-                            name={t(FRIDGE)}
-                            component={FridgeStackScreen}
-                            options={{
-                              tabBarBadge: countExpiredItems(productsList),
-                            }}
-                          />
-                          <Tab.Screen
-                            name={t(FREEZER)}
-                            component={FreezerStackScreen}
-                          />
-                          <Tab.Screen
-                            name={t(SETTINGS)}
-                            component={SettingsStackScreen}
-                          />
-                        </Tab.Navigator>
+                        TabNavigator(productsList, t, primary)
                       ) : (
                         <SingInStackScreen />
                       )}
@@ -80,13 +39,4 @@ export default function AppContainer() {
   );
 }
 
-const tabBarOptions = {
-  inactiveTintColor: "black",
-  showIcon: true,
-  showLabel: true,
-  upperCaseLabel: true,
-  labelStyle: {
-    fontFamily: "OpenSans-Regular",
-    textTransform: "uppercase",
-  },
-};
+export default memo(AppContainer);
