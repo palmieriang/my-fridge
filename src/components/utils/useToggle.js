81c5aec93a630dc8612d33729b7484d840c88061
asos-angelopalmieri <angelo.palmieri@asos.com>
2024-04-05 12:44:10 +0100

    lint

--- src/components/utils/useToggle.js
+++ src/components/utils/useToggle.js
@@ -1,10 +1,8 @@
-import React, { useState, useCallback } from "react";
+import { useState, useCallback } from "react";
 
 const useToggle = (initialState) => {
   const [isToggled, setIsToggled] = useState(initialState);
 
-  // put [setIsToggled] into the useCallback's dependencies array
-  // this value never changes so the callback is not going to be ever re-created
   const toggle = useCallback(
     () => setIsToggled((state) => !state),
     [setIsToggled],
