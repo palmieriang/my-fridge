eaa7e5430ff0cdac9e3afe8f5300b4c6a78054fd
asos-angelopalmieri <angelo.palmieri@asos.com>
2024-10-06 12:13:41 +0100

    Settings to ts

--- src/screens/Settings/Settings.tsx
+++ src/screens/Settings/Settings.tsx
@@ -1,10 +1,18 @@
 import React, { useContext } from "react";
 import { Alert, Button, Text, View } from "react-native";
-import ModalSelector from "react-native-modal-selector";
+import ModalSelector, { IOption } from "react-native-modal-selector";
 
 import styles from "./styles";
 import { authStore, localeStore, themeStore } from "../../store";
 import Profile from "../Profile/Profile";
+
+interface LanguageOption extends IOption {
+  section?: boolean;
+}
+
+interface ThemeOption extends IOption {
+  section?: boolean;
+}
 
 const Settings = () => {
   const {
@@ -19,7 +27,7 @@ const Settings = () => {
     themeContext: { changeTheme },
   } = useContext(themeStore);
 
-  const languageData = [
+  const languageData: LanguageOption[] = [
     { section: true, label: t("chooseLanguage"), key: "title" },
     { label: t("english"), value: "en", key: "english" },
     { label: t("spanish"), value: "es", key: "spanish" },
@@ -28,7 +36,7 @@ const Settings = () => {
     { label: t("portuguese"), value: "pt", key: "portuguese" },
   ];
 
-  const themeData = [
+  const themeData: ThemeOption[] = [
     { section: true, label: t("changeTheme"), key: "title" },
     { label: "Light Red", value: "lightRed", key: "lightRed" },
     { label: "Light Blue", value: "lightBlue", key: "lightBlue" },
@@ -36,12 +44,12 @@ const Settings = () => {
     { label: "Dark Blue", value: "darkBlue", key: "darkBlue" },
   ];
 
-  const handleLocale = (locale) => {
+  const handleLocale = (locale: LanguageOption) => {
     const newLocale = locale.value;
     changeLocale({ newLocale, id });
   };
 
-  const handleTheme = (theme) => {
+  const handleTheme = (theme: ThemeOption) => {
     const newTheme = theme.value;
     changeTheme({ newTheme, id });
   };
