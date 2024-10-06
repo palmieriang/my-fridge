986b955ea1f13fc42d4b323d48f19679ba0b8fa8
asos-angelopalmieri <angelo.palmieri@asos.com>
2024-10-06 10:04:55 +0100

    ProductList updated

--- src/navigation/navigation.d.ts
+++ src/navigation/navigation.d.ts
@@ -3,7 +3,7 @@ import { StackNavigationProp } from "@react-navigation/stack";
 
 export type RootStackParamList = {
   list: { place: string };
-  form: { id: string; product: any; title: string };
+  form: { id?: string; product?: any; title: string };
   settings: undefined;
   signin: undefined;
   registration: undefined;
@@ -15,3 +15,10 @@ export type FormScreenNavigationProp = StackNavigationProp<
 >;
 
 export type FormScreenRouteProp = RouteProp<RootStackParamList, "form">;
+
+export type ProductListNavigationProp = StackNavigationProp<
+  RootStackParamList,
+  "list"
+>;
+
+export type ProductListRouteProp = RouteProp<RootStackParamList, "list">;
