0a6c695b55ad929457330bfa4179ce338dc7378b
asos-angelopalmieri <angelo.palmieri@asos.com>
2025-07-06 23:59:01 +0100

    Remove unused imports and commented-out styles from App component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import "react-native-gesture-handler";
 import { registerRootComponent } from "expo";
 import { useFonts } from "expo-font";
 import React, { useEffect, useState } from "react";
-// import { View, ActivityIndicator, StyleSheet, Text } from "react-native";
 
 import AppContainer from "./AppContainer";
 import { initializeFirebaseServices } from "./firebase/config";
@@ -33,13 +32,5 @@ export default function App() {
 
   return <AppContainer />;
 }
-
-// const styles = StyleSheet.create({
-//   loadingContainer: {
-//     flex: 1,
-//     justifyContent: "center",
-//     alignItems: "center",
-//   },
-// });
 
 registerRootComponent(App);
