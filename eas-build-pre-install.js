f9dec62a381bd499e22d6bf9f70fb1d4bc336332
asos-angelopalmieri <angelo.palmieri@asos.com>
2025-07-08 17:33:32 +0100

    Refactor google-services.json and GoogleService-Info.plist restoration logic for improved clarity and consistency

--- eas-build-pre-install.js
+++ eas-build-pre-install.js
@@ -13,4 +13,20 @@ if (process.env.GOOGLE_SERVICES_JSON) {
     process.env.GOOGLE_SERVICES_JSON,
     "./android/app/google-services.json",
   );
+} else {
+  console.warn(
+    "GOOGLE_SERVICES_JSON environment variable not found. Skipping google-services.json restoration.",
+  );
+}
+
+if (process.env.GOOGLE_SERVICE_INFO_PLIST) {
+  console.log("Restoring GoogleService-Info.plist from secret...");
+  decodeBase64ToFile(
+    process.env.GOOGLE_SERVICE_INFO_PLIST,
+    "./ios/GoogleService-Info.plist",
+  );
+} else {
+  console.warn(
+    "GOOGLE_SERVICE_INFO_PLIST environment variable not found. Skipping GoogleService-Info.plist restoration.",
+  );
 }
