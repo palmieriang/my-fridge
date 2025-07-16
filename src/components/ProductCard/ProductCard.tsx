dc4ea99d7c90244ae098bf678aac5b9f62ccf9c5
asos-angelopalmieri <angelo.palmieri@asos.com>
2025-07-16 22:45:06 +0100

    Fixed types for ProductCard

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -1,6 +1,7 @@
 import { useNavigation } from "@react-navigation/native";
 import React, { useContext, useEffect, useState, forwardRef } from "react";
 import { Text, TouchableWithoutFeedback, View } from "react-native";
+import { Swipeable } from "react-native-gesture-handler";
 
 import styles from "./styles";
 import { FRIDGE, FREEZER } from "../../constants";
@@ -18,86 +19,90 @@ type ProductCardProps = {
   };
 };
 
-const ProductCard = forwardRef(({ product }: ProductCardProps, ref) => {
-  const { date, id, name, place } = product;
+const ProductCard = forwardRef<Swipeable, ProductCardProps>(
+  ({ product }, ref) => {
+    const { date, id, name, place } = product;
 
-  const [expired, setExpired] = useState(false);
+    const [expired, setExpired] = useState(false);
 
-  const { navigate } = useNavigation<FormScreenNavigationProp>();
+    const { navigate } = useNavigation<FormScreenNavigationProp>();
 
-  const {
-    localizationContext: { t },
-  } = useContext(localeStore);
-  const { theme } = useContext(themeStore);
-  const { productsContext } = useContext(productsStore);
+    const {
+      localizationContext: { t },
+    } = useContext(localeStore);
+    const { theme } = useContext(themeStore);
+    const { productsContext } = useContext(productsStore);
 
-  const days = daysUntilDate(date);
+    const days = daysUntilDate(date);
 
-  useEffect(() => {
-    if (days < 0) {
-      setExpired(true);
-    } else {
-      setExpired(false);
-    }
-  });
+    useEffect(() => {
+      if (days < 0) {
+        setExpired(true);
+      } else {
+        setExpired(false);
+      }
+    });
 
-  const handleChange = () => {
-    productsContext
-      .handleGetProduct(id)
-      .then((product) => {
-        navigate("form", {
-          id,
-          product,
-          title: t("modifyItem"),
-        });
-      })
-      .catch((error) => console.log("Error: ", error));
-  };
+    const handleChange = () => {
+      productsContext
+        .handleGetProduct(id)
+        .then((product) => {
+          navigate("form", {
+            id,
+            product,
+            title: t("modifyItem"),
+          });
+        })
+        .catch((error) => console.log("Error: ", error));
+    };
 
-  const handleFreeze = () => {
-    const moveTo = place === FRIDGE ? FREEZER : FRIDGE;
-    productsContext.handleFreezeProduct(id, moveTo);
-  };
+    const handleFreeze = () => {
+      const moveTo = place === FRIDGE ? FREEZER : FRIDGE;
+      productsContext.handleFreezeProduct(id, moveTo);
+    };
 
-  const handleDelete = () => {
-    productsContext.handleDeleteProduct(id);
-  };
+    const handleDelete = () => {
+      productsContext.handleDeleteProduct(id);
+    };
 
-  return (
-    <SwipeableRow
-      ref={ref}
-      modifyFunction={handleChange}
-      deleteFunction={handleDelete}
-      freezeFunction={handleFreeze}
-      place={place}
-    >
-      <View>
-        <TouchableWithoutFeedback onPress={handleChange}>
-          <View style={[styles.card, { backgroundColor: theme.foreground }]}>
-            <View>
-              <Text style={[styles.date, { color: theme.text }]}>{date}</Text>
-              <Text style={[styles.title, { color: theme.text }]}>{name}</Text>
-            </View>
-
-            {expired ? (
-              <Text style={[styles.expired, { color: theme.primary }]}>
-                {t("expired")}
-              </Text>
-            ) : (
-              <View style={styles.counterContainer}>
-                <Text style={[styles.counterText, { color: theme.primary }]}>
-                  {days}
-                </Text>
-                <Text style={[styles.counterLabel, { color: theme.text }]}>
-                  {t("days").toUpperCase()}
+    return (
+      <SwipeableRow
+        ref={ref}
+        modifyFunction={handleChange}
+        deleteFunction={handleDelete}
+        freezeFunction={handleFreeze}
+        place={place}
+      >
+        <View>
+          <TouchableWithoutFeedback onPress={handleChange}>
+            <View style={[styles.card, { backgroundColor: theme.foreground }]}>
+              <View>
+                <Text style={[styles.date, { color: theme.text }]}>{date}</Text>
+                <Text style={[styles.title, { color: theme.text }]}>
+                  {name}
                 </Text>
               </View>
-            )}
-          </View>
-        </TouchableWithoutFeedback>
-      </View>
-    </SwipeableRow>
-  );
-});
+
+              {expired ? (
+                <Text style={[styles.expired, { color: theme.primary }]}>
+                  {t("expired")}
+                </Text>
+              ) : (
+                <View style={styles.counterContainer}>
+                  <Text style={[styles.counterText, { color: theme.primary }]}>
+                    {days}
+                  </Text>
+                  <Text style={[styles.counterLabel, { color: theme.text }]}>
+                    {t("days").toUpperCase()}
+                  </Text>
+                </View>
+              )}
+            </View>
+          </TouchableWithoutFeedback>
+        </View>
+      </SwipeableRow>
+    );
+  },
+);
 
 export default ProductCard;
