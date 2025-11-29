87f1c26853627890eb2a118df2663e2f7c91d9a1
asos-angelopalmieri <angelo.palmieri@asos.com>
2025-11-29 15:11:22 +0000

    refactor: Simplify file restoration logic for Google services configuration

--- eas-build-pre-install.js
+++ eas-build-pre-install.js
@@ -7,26 +7,23 @@ function decodeBase64ToFile(encoded, filePath) {
   fs.writeFileSync(filePath, buffer);
 }
 
+const androidConfigPath = "./android/app/google-services.json";
+const iosConfigPath = "./ios/GoogleService-Info.plist";
+
 if (process.env.GOOGLE_SERVICES_JSON) {
   console.log("Restoring google-services.json from secret...");
-  decodeBase64ToFile(
-    process.env.GOOGLE_SERVICES_JSON,
-    "./android/app/google-services.json",
-  );
-} else {
+  decodeBase64ToFile(process.env.GOOGLE_SERVICES_JSON, androidConfigPath);
+} else if (!fs.existsSync(androidConfigPath)) {
   console.warn(
-    "GOOGLE_SERVICES_JSON environment variable not found. Skipping google-services.json restoration.",
+    "GOOGLE_SERVICES_JSON environment variable not found and file doesn't exist locally.",
   );
 }
 
 if (process.env.GOOGLE_SERVICE_INFO_PLIST) {
   console.log("Restoring GoogleService-Info.plist from secret...");
-  decodeBase64ToFile(
-    process.env.GOOGLE_SERVICE_INFO_PLIST,
-    "./ios/GoogleService-Info.plist",
-  );
-} else {
+  decodeBase64ToFile(process.env.GOOGLE_SERVICE_INFO_PLIST, iosConfigPath);
+} else if (!fs.existsSync(iosConfigPath)) {
   console.warn(
-    "GOOGLE_SERVICE_INFO_PLIST environment variable not found. Skipping GoogleService-Info.plist restoration.",
+    "GOOGLE_SERVICE_INFO_PLIST environment variable not found and file doesn't exist locally.",
   );
 }
